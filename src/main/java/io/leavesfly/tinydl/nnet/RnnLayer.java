7d2d10b3bfeccf299dbe47d874ee70fa9c4816b6
山泽 <yefei.yf@alibaba-inc.com>
2025-09-22 10:09:23 +0800

    rnn

--- src/main/java/io/leavesfly/tinydl/nnet/RnnLayer.java
+++ src/main/java/io/leavesfly/tinydl/nnet/RnnLayer.java
@@ -1,1 +1,15 @@
- 
+package io.leavesfly.tinydl.nnet;
+
+import io.leavesfly.tinydl.ndarr.Shape;
+
+/**
+ * 递归神经网络层，区别于普通的前馈网络，有状态
+ */
+public abstract class RnnLayer extends Layer {
+    public RnnLayer(String _name, Shape _inputShape, Shape _outputShape) {
+        super(_name, _inputShape, _outputShape);
+    }
+
+    public abstract void resetState();
+
+}
